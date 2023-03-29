5d4ed5eb7290caa1966f1a96a53328e4ace71d05
Ken VanDine <ken.vandine@canonical.com>
2023-03-29 16:42:43 -0400

    Cleaned up download script to make it more readable

--- kramden/22.04/scripts/snaps.sh
+++ kramden/22.04/scripts/snaps.sh
@@ -10,3 +10,4 @@ for i in /home/snaps/core*.snap; do snap install $i;done
 for i in /home/snaps/gtk*.snap; do snap install $i;done
 for i in /home/snaps/gnome-*.snap; do snap install $i;done
 for i in /home/snaps/{firefox,chromium,vlc,zoom-client,snapd-desktop-integration,snap-store}*.snap; do snap install $i;done
+for i in /home/snaps/{mc-installer,aqueducts}*.snap; do snap install $i;done
--- kramden/snaps/download.sh
+++ kramden/snaps/download.sh
@@ -4,18 +4,30 @@ old=$PWD
 echo $old
 
 cd `dirname $0`
-for i in gnome-42-2204 gnome-3-38-2004 firefox snapd-desktop-integration zoom-client vlc core18 core22 gtk-common-themes; do
+
+#Download Ubuntu seeded snaps
+for i in gnome-42-2204 gnome-3-38-2004 firefox snapd-desktop-integration core22 gtk-common-themes; do
         snap download --channel=stable $i
 done
 
 snap download --channel=preview/edge snap-store
 
+#Download kramden specific snaps
+for i in zoom-client vlc core18; do
+        snap download --channel=stable $i
+done
+
+#Download kramden specific games
+for i in aqueducts mc-installer; do
+        snap download --channel=stable $i
+done
+
 cd ..
 
 tar cvf snaps.tar snaps
 if [ $? -eq 0 ]; then
-	echo "snaps.tar archive created"
-	rm snaps/*.assert snaps/*.snap
+        echo "snaps.tar archive created"
+        rm snaps/*.assert snaps/*.snap
 fi
 
 cd $old
