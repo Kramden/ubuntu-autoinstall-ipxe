923a28b5fbbe28cd3b35fd0ceffcddec75ae8d21
Marshall Burkes <mburkes@kramden.org>
2023-05-10 17:47:47 -0400

    Update snaps.sh
    Add same additional new snaps as listed in snaps/downloads.sh

--- kramden/22.04/scripts/snaps.sh
+++ kramden/22.04/scripts/snaps.sh
@@ -11,3 +11,4 @@ for i in /home/snaps/gtk*.snap; do snap install $i;done
 for i in /home/snaps/gnome-*.snap; do snap install $i;done
 for i in /home/snaps/{firefox,chromium,vlc,zoom-client,snapd-desktop-integration,snap-store}*.snap; do snap install $i;done
 for i in /home/snaps/{mc-installer,aqueducts}*.snap; do snap install $i;done
+for i in /home/snaps/{0ad,audacity,blender,darktable,dragonsapprentice,fablemaker,frogsquash,gcompris,gnome-chess,gnome-dictionary,gnome-nibbles,gnome-weather,iagno,inkscape,kalzium,kdenlive,kgeography,kolourpaint,krita,ktouch,lazpaint,leocad,mapton,marsshooter,memory-tiles,micropolis,midnightmareteddy,missilemath,open-typer,paintsupreme-3d,pinta,pixelorama,quadrapassel,spotify,steam,stellarium-daily,supertux,supertuxkart,sweethome3d-homedesign,tank-warriors,thepassage,tux-paint,tuxtyping,white-house}*.snap; do snap install $i;done
