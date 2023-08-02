abe9f5300b1e3129b4749e758c36e80590449a2a
Ken VanDine <ken.vandine@canonical.com>
2023-08-02 19:15:53 -0400

    Refactor snap download script and installation script

--- kramden/22.04/scripts/snaps.sh
+++ kramden/22.04/scripts/snaps.sh
@@ -3,13 +3,11 @@
 cd /home && tar xvf snaps.tar
 
 # ack all the snaps
-for i in /home/snaps/*.assert; do snap ack $i;done
+for i in /home/snaps/first/*.assert; do snap ack $i;done
+for i in /home/snaps/second/*.assert; do snap ack $i;done
 
 # side-load the snaps to bootstrap
-for i in /home/snaps/core*.snap; do snap install $i;done
-for i in /home/snaps/gtk*.snap; do snap install $i;done
-for i in /home/snaps/gnome-*.snap; do snap install $i;done
-snap install gaming-graphics-core22*.snap
-for i in /home/snaps/{firefox,chromium,vlc,zoom-client,snapd-desktop-integration,snap-store}*.snap; do snap install $i;done
-for i in /home/snaps/{mc-installer,aqueducts}*.snap; do snap install $i;done
-for i in /home/snaps/{0ad,audacity,blender,darktable,dragonsapprentice,fablemaker,frogsquash,gcompris,gnome-chess,gnome-dictionary,gnome-nibbles,gnome-weather,iagno,inkscape,kalzium,kdenlive,kgeography,kolourpaint,krita,ktouch,lazpaint,leocad,mapton,marsshooter,memory-tiles,micropolis,midnightmareteddy,missilemath,open-typer,paintsupreme-3d,pinta,pixelorama,quadrapassel,spotify,steam,stellarium-daily,supertux,supertuxkart,sweethome3d-homedesign,tank-warriors,thepassage,tux-paint,tuxtyping,white-house}*.snap; do snap install $i;done
+for i in /home/snaps/first/core*.snap; do snap install $i;done
+for i in /home/snaps/first/gtk*.snap; do snap install $i;done
+for i in /home/snaps/first/gnome-*.snap; do snap install $i;done
+for i in /home/snaps/second/*.snap; do snap install $i;done
--- kramden/22.04/snaps/download.sh
+++ kramden/22.04/snaps/download.sh
@@ -2,21 +2,36 @@
 
 old=$PWD
 echo $old
+dir=$(dirname $(realpath snaps/download.sh))
 
-cd `dirname $0`
+cd $dir
 
-#Download Ubuntu seeded snaps
-for i in gnome-42-2204 gnome-3-38-2004 snapd-desktop-integration core22 gtk-common-themes; do
-        snap download --channel=stable $i
+echo `pwd`
+mkdir $dir/first && cd $dir/first
+echo `pwd`
+#Download base and content snaps
+for i in gnome-42-2204 gnome-3-38-2004 gtk-common-themes; do
+        snap download --channel=stable/ubuntu-22.04 $i
 done
 
+snap download --channel=stable core22
+
+#Needed for vlc
+#snap download --channel=stable core18
+
+cd $dir
+echo `pwd`
+mkdir $dir/second && cd $dir/second
+echo `pwd`
+
+snap download --channel=stable/ubuntu-22.04 snapd-desktop-integration
 snap download --channel=preview/edge snap-store
 snap download --channel=stable/ubuntu-23.04 firefox
 
 #Download kramden specific snaps
-for i in zoom-client vlc core18; do
-        snap download --channel=stable $i
-done
+#for i in zoom-client vlc; do
+#        snap download --channel=stable $i
+#done
 
 #Download kramden specific games
 for i in gnome-2048 quadrapassel gnome-chess gnome-nibbles frogsquash mc-installer iagno; do
@@ -28,12 +43,12 @@ for i in spotify gnome-weather gnome-dictionary; do
         snap download --channel=stable $i
 done
 
-cd ..
+cd $dir/..
 
 tar cvf snaps.tar snaps
 if [ $? -eq 0 ]; then
         echo "snaps.tar archive created"
-        rm snaps/*.assert snaps/*.snap
+        rm -rf snaps/{first,second}
 fi
 
 cd $old
