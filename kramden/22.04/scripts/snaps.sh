388041ffa594e86cc97abd3d93290637febd80bc
Ken VanDine <ken.vandine@canonical.com>
2023-10-11 19:09:28 -0400

    set tracking branches at install time

--- kramden/22.04/scripts/snaps.sh
+++ kramden/22.04/scripts/snaps.sh
@@ -12,4 +12,10 @@ for i in /home/snaps/first/gtk*.snap; do snap install $i;done
 for i in /home/snaps/first/gnome-*.snap; do snap install $i;done
 for i in /home/snaps/second/*.snap; do snap install $i;done
 
+snap switch --channel=latest/stable/22.04 core22
+snap switch --channel=latest/stable/22.04 gtk-common-themes
+snap switch --channel=latest/stable/22.04 gnome-42-2204
+snap switch --channel=latest/stable/22.04 snapd-desktop-integration
+snap switch --channel=latest/stable/22.04 firefox
+snap switch --channel=latest/stable/23.10 snap-store
 snap remove --purge lxd
